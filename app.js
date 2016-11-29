7549c535d2c4c265b668c47f5bb3ad96ca3f3582
JEFF BLANCHARD <BLANCHARDJD19@COMCAST.NET>
2016-11-29 14:56:05 -0800

    Changed the variable name from question to interaction.

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 'use strict';
-/* Design a guessing game about you that involves FIVE yes/no questions. Be creative
+/* Design a guessing game about you that involves FIVE yes/no responses. Be creative
 and fun and whimsical. Or not. It's up to you! It's your project!
-The questions in the guessing game must require a mix of yes/no answers,
+The responses in the guessing game must require a mix of yes/no answers,
 and user input must accept either y/n or yes/no responses, with either
 .toUpperCase() or .toLowerCase() used to validate the user input
 and accommodate users entering all-caps Y/N or YES/NO answers, too.
 Useful and descriptive console.log() messages in the JS are well written
- and correctly displaying to the browser console for each question of the guessing game.
-var question1 = prompt ("Do I watch sports?")*/
-var question1 = prompt( 'Do I watch football?').toLowerCase();
- if (question1 === 'yes' || question1 === 'y') {
+ and correctly displaying to the browser console for each response of the guessing game.
+var response1 = prompt ("Do I watch sports?")*/
+var interaction1 = prompt( 'Do I watch football?').toLowerCase();
+ if (interaction1 === 'yes' || interaction1 === 'y') {
    alert(' Wrong answer yo!');
-   else if (question1 === 'no' || question1 === 'n'){
+   else if (interaction1 === 'no' || interaction1 === 'n'){
     alert('You are correct, you know me so well ;) ');
 }
 
 } else {
-}   alert('It is a yes or no question there ace.');
+}   alert('It is a yes or no response there ace.');
 
-var question2 = prompt(' Have I been to Indonesia? ').toLowerCase();
-if (question2 === 'yes' || question2 === 'y') {
+var interaction2 = prompt(' Have I been to Indonesia? ').toLowerCase();
+ if (interaction2 === 'yes' || interaction2 === 'y') {
   alert('I have indeed! Twice actually.');
-
-} else {
+} else if (interaction2 === 'no' || interaction2 === 'n'){
   alert(' Guess again sucka! ');
+} else {
+   alert('Really again? It is still a yes or no answer');
 }
