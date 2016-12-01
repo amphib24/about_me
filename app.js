2640210c7cf3b3997e28c81b41e11c236a21ddab
JEFF BLANCHARD <BLANCHARDJD19@COMCAST.NET>
2016-11-30 16:24:00 -0800

    Added a while loop for the last question

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+var correctResponse = 0;
 var alertUser = alert('You are about to embark on a journey of getting to know random facts about me, Jeff.');
 alert('These are yes or no answers so please answer by entering yes, no, n, or y. ENJOY the Journey! ');
 var interaction1 = prompt( 'Do I watch football?').toLowerCase();
@@ -7,6 +8,7 @@ var interaction1 = prompt( 'Do I watch football?').toLowerCase();
    alert(' Wrong answer yo!');
  }else if (interaction1 === 'no' || interaction1 === 'n'){
     alert('You are correct, you know me so well ;) ');
+    correctResponse++;
 } else {
    alert('It is a yes or no response there ace.');
 }
@@ -17,6 +19,7 @@ var interaction2 = prompt(' Have I been to Indonesia? ').toLowerCase();
 /*answer = yes*/
  if (interaction2 === 'yes' || interaction2 === 'y') {
   alert('I have indeed! Twice actually.');
+  correctResponse++;
 } else if (interaction2 === 'no' || interaction2 === 'n'){
   alert(' Guess again sucka! ');
 } else {
@@ -31,6 +34,7 @@ var interaction3 = prompt('When I was 10 did I fall 3 stories into a pile of lea
    alert('Hell no that would hurt! Pay me  $100');
  } else if (interaction3 === 'no' || interaction3 === 'n') {
    alert('You are correct! ');
+   correctResponse++;
  } else {
    prompt('It is still a yes or no answer......');
  }
@@ -41,6 +45,7 @@ var interaction3 = prompt('When I was 10 did I fall 3 stories into a pile of lea
   /* answer = yes*/
   if (interaction4 === 'yes' || interaction4 === 'y'){
     alert('I sure have. Winner got free drinks... she won everytime');
+    correctResponse++;
  }  else if (interaction4 === 'no' || interaction4 === 'n'){
     alert('Seems strange but I did!');
  } else {
@@ -55,6 +60,7 @@ var interaction3 = prompt('When I was 10 did I fall 3 stories into a pile of lea
     alert('No. I always ended up getting bored.');
   }else if(interaction5 === 'no' || interaction5 === 'n'){
     alert( 'Great guess. You get a participation medal!')
+    correctResponse++;
   } else {
 alert('Answer with some form of yes or no.');
   }
@@ -72,13 +78,37 @@ for ( var i = 0; i < 4; i++) {
   if (guess === myNumber) {
     alert('Great guess! You get a participation medal! ')
     i=4;
+    correctResponse++;
   }
-  if ( i === 3){
+  if ( i === 3) {
    alert('Me so sorry but you are out of guesses. Feel free to refresh the page and try again. ');
   }
 }
 console.log('User finished guessing game.');
 /*space between questions*/
 
+var maxAttempts = 6
+var countriesVisited =
+ ['Indonesia','Phillipines','Singapore','Japan','Thailand'];
+var correctAnswer = false;
+  while (maxAttempts > 0){
+    var interaction7 = prompt('Can you guess one country I have been to? ');
+  for (var i = 0; i < countriesVisited.length; i++){
+    var country = countriesVisited[i];
+    if (interaction7 === country){
+    alert('Great Job!');
+    correctAnswer = true;
+    correctResponse++;
+    break;
+      }
+  }
+  maxAttempts -= 1;
+   if (maxAttempts === 0){
+    alert('Sorry guesses are up. The answers were Indonesia, Phillipines, Singapore, Japan, and Thailand');
+     }
+  if (correctAnswer === true){
+  break;
+    }
+}
   /*closing alert*/
-var closingStatement = alert('MIND BLOWN! Thanks for taking time to play. Please take sometime to read through my page to learn more about my professinal goals and history. ');
+var closingStatement = alert('MIND BLOWN! You got ' + correctResponse + ' out of 7. Thanks for taking time to play. Please take sometime to read through my page to learn more about my professinal goals and history. ');
